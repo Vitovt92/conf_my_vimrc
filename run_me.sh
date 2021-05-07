2a763b66f5b2bf38fff5f726e7ace882d31b8afe
vitovt <sergeevvs92@gmail.com>
2021-05-07 18:19:34 +0300

    1. Copy my actual .vimrc to vimrc.txt 2. Create initial bash scripts run_me.sh that create needed folders

---
+++ run_me.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "copy vimrc"
+cat vimrc.txt > ~/.vimrc
+
+if [ ! -d ~/.vim  ]
+then
+    echo "create .vim"
+    mkdir ~/.vim
+fi
+
+echo "create ~/.vim/swp  ~/.vim/undodir"
+mkdir ~/.vim/swp  ~/.vim/undodir 
+
